d5763b50ba86dc44f9e4fff5e4dda526b7ebf286
Ramin Zamani <ramin@lizard.global>
2023-06-01 13:36:42 +0800

    update serializer

--- core/user_api/serializers.py
+++ core/user_api/serializers.py
@@ -11,14 +11,19 @@ class UserApiPermissionNameSerializer(serializers.ModelSerializer):
 
 
 class UserAPIPermissionSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(source='user_api')
+    user_id = serializers.IntegerField(source='user_api.id')
+    permission_name = serializers.CharField(source='user_api_permission_name')
+    permission_name_id = serializers.IntegerField(source='user_api_permission_name.id')
+    user_permission_id = serializers.IntegerField(source='id')
     class Meta:
         model = UserApiPermission
-        exclude = ['customre', 'created', 'updated_at']
+        exclude = ['customre', 'created', 'updated_at', 'id', 'user_api_permission_name', 'user_api']
 
 
 
 class UserApiSerializer(serializers.ModelSerializer):
-    user_api = UserAPIPermissionSerializer(many = True)
+    permissions = UserAPIPermissionSerializer(many = True, source='user_api')
     class Meta:
         model = UserApi
         exclude = ['customre', 'created', 'updated_at', 'user_password']
